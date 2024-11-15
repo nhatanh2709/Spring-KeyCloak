29d1a12cd08d968b752c7a709306a98c8e12f856
nhatanh2709 <22520059@gm.uit.edu.vn>
2024-11-16 01:45:25 +0700

    Initial KeyCloak

---
+++ profile-service/src/main/java/com/devteria/profile/dto/identity/KeyCloakError.java
@@ -0,0 +1,15 @@
+package com.devteria.profile.dto.identity;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class KeyCloakError {
+    String errorMessage;
+}
